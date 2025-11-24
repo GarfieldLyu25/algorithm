d0bc30071a8d353745cc1ed53b5eb99ed5917e23
Garfield Lyu <lv2895045412@gmail.com>
2025-11-24 11:49:09 +0800

    1124

---
+++ sql/leetcode626.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT (
+    CASE
+        WHEN id%2=0 THEN id-1
+        WHEN id%2=1 AND id<(SELECT max(id) FROM Seat) THEN id+1
+        ELSE id
+    END
+) id, student FROM Seat
+ORDER BY id
